0791f15b1ff484f707c01635a440bad0d099127c
jeanpier_monserrate <jeanpier.dev@gmail.com>
2022-04-16 13:49:06 -0400

    updating public assets

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+dotenv_1.default.config({ path: `./${process.env.ENV}.env` });
+;
+console.log(process.env.ENV);
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use(express_1.default.static('public'));
--- index.ts
+++ index.ts
@@ -1,13 +1,14 @@
 import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: `./${process.env.ENV}.env` }));
+console.log(process.env.ENV);
 
 const app: Express = express();
 const port = process.env.PORT;
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
